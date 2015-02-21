1bc266d457fb6c89dc3bd62f4de5d4a440d6e3d2
Christian Rudolph <christian+git@famiru.de>
2015-02-21 11:19:41 +0100

    additional checks for type safety

--- thymeleaf-extras-springsecurity4/src/main/java/org/thymeleaf/extras/springsecurity4/auth/AuthUtils.java
+++ thymeleaf-extras-springsecurity4/src/main/java/org/thymeleaf/extras/springsecurity4/auth/AuthUtils.java
@@ -36,6 +36,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.GenericTypeResolver;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ParseException;
@@ -58,7 +59,7 @@ import org.thymeleaf.util.Validate;
 
 
 /**
- * 
+ *
  * @author Daniel Fern&aacute;ndez
  *
  */
@@ -66,31 +67,31 @@ public final class AuthUtils {
 
 
     private static final Logger logger = LoggerFactory.getLogger(AuthUtils.class);
-    
+
     private static final FilterChain DUMMY_CHAIN = new FilterChain() {
-        public void doFilter(ServletRequest request, ServletResponse response) 
+        public void doFilter(ServletRequest request, ServletResponse response)
                 throws IOException, ServletException {
            throw new UnsupportedOperationException();
         }
     };
 
-    
-    
-    
+
+
+
     private AuthUtils() {
         super();
     }
 
-    
-    
+
+
 
     public static Authentication getAuthenticationObject() {
-        
+
         if (logger.isTraceEnabled()) {
             logger.trace("[THYMELEAF][{}] Obtaining authentication object.",
                     new Object[] {TemplateEngine.threadIndex()});
         }
-        
+
         if ((SecurityContextHolder.getContext() == null)) {
             if (logger.isTraceEnabled()) {
                 logger.trace("[THYMELEAF][{}] No security context found, no authentication object returned.",
@@ -99,7 +100,7 @@ public final class AuthUtils {
             return null;
         }
 
-        final Authentication authentication = 
+        final Authentication authentication =
                 SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null || authentication.getPrincipal() == null) {
@@ -109,63 +110,63 @@ public final class AuthUtils {
             }
             return null;
         }
-        
+
         if (logger.isTraceEnabled()) {
             logger.trace("[THYMELEAF][{}] Authentication object of class {} found in context for user \"{}\".",
                     new Object[] {TemplateEngine.threadIndex(), authentication.getClass().getName()}, authentication.getName());
         }
-        
+
         return authentication;
-        
+
     }
 
-    
+
 
     public static Object getAuthenticationProperty(final Authentication authentication, final String property) {
-        
+
         if (logger.isTraceEnabled()) {
             logger.trace("[THYMELEAF][{}] Reading property \"{}\" from authentication object.",
                     new Object[] {TemplateEngine.threadIndex(), property});
         }
-        
+
         if (authentication == null) {
             return null;
         }
-        
+
         try {
-            
+
             final BeanWrapperImpl wrapper = new BeanWrapperImpl(authentication);
             final Object propertyObj = wrapper.getPropertyValue(property);
-            
+
             if (logger.isTraceEnabled()) {
                 logger.trace("[THYMELEAF][{}] Property \"{}\" obtained from authentication object " +
                 		"for user \"{}\". Returned value of class {}.",
-                        new Object[] {TemplateEngine.threadIndex(), property, authentication.getName(), 
+                        new Object[] {TemplateEngine.threadIndex(), property, authentication.getName(),
                                 (propertyObj == null? null : propertyObj.getClass().getName())});
             }
-            
+
             return propertyObj;
-            
+
         } catch (BeansException e) {
             throw new TemplateProcessingException(
-                    "Error retrieving value for property \"" + property + "\" of authentication " + 
+                    "Error retrieving value for property \"" + property + "\" of authentication " +
                     "object of class " + authentication.getClass().getName(), e);
         }
 
     }
-    
-    
-    
-    
+
+
+
+
 
     public static boolean authorizeUsingAccessExpression(
             final IProcessingContext processingContext,
-            final String accessExpression, final Authentication authentication, 
+            final String accessExpression, final Authentication authentication,
             final HttpServletRequest request, final HttpServletResponse response,
             final ServletContext servletContext) {
-    
+
         Validate.notNull(processingContext, "Processing context cannot be null");
-        
+
         if (logger.isTraceEnabled()) {
             logger.trace("[THYMELEAF][{}] Checking authorization using access expression \"{}\" for user \"{}\".",
                     new Object[] {TemplateEngine.threadIndex(), accessExpression, (authentication == null? null : authentication.getName())});
@@ -178,26 +179,25 @@ public final class AuthUtils {
                 ((accessExpression != null && accessExpression.startsWith("${") && accessExpression.endsWith("}"))?
                         accessExpression.substring(2, accessExpression.length() - 1) :
                         accessExpression);
-        
-        final SecurityExpressionHandler<? super FilterInvocation> handler =
-                (SecurityExpressionHandler<? super FilterInvocation>) getExpressionHandler(servletContext);
+
+        final SecurityExpressionHandler<FilterInvocation> handler = getExpressionHandler(servletContext);
 
         Expression expressionObject = null;
         try {
             expressionObject = handler.getExpressionParser().parseExpression(expr);
         } catch (ParseException e) {
             throw new TemplateProcessingException(
-                    "An error happened trying to parse Spring Security access expression \"" +  
+                    "An error happened trying to parse Spring Security access expression \"" +
                     expr + "\"", e);
         }
 
         final FilterInvocation filterInvocation = new FilterInvocation(request, response, DUMMY_CHAIN);
-        
+
         final EvaluationContext evaluationContext = handler.createEvaluationContext(authentication, filterInvocation);
-        
+
         /*
          * Initialize the context variables map.
-         * 
+         *
          * This will allow SpringSecurity expressions to include any variables from
          * the IContext just by accessing them as properties of the "#vars" utility object.
          */
@@ -206,7 +206,7 @@ public final class AuthUtils {
             // Try to initialize the context variables by asking the SpEL expression
             // evaluator object (which might be a subclass of the standard one) to create
             // this variable map.
-            
+
             final Arguments arguments = (Arguments) processingContext;
             final IStandardVariableExpressionEvaluator expressionEvaluator =
                     StandardExpressions.getVariableExpressionEvaluator(arguments.getConfiguration());
@@ -215,83 +215,86 @@ public final class AuthUtils {
                     SpringVersionSpecificUtils.computeExpressionObjectsFromExpressionEvaluator(arguments, expressionEvaluator);
 
         }
-        
+
         if (contextVariables == null) {
             // if we could not create it the more integrated way, just do it the hard-wired way
             contextVariables = new HashMap<String, Object>();
-            
+
             final Map<String,Object> expressionObjects = processingContext.getExpressionObjects();
             if (expressionObjects != null) {
                 contextVariables.putAll(expressionObjects);
             }
-            
+
         }
-        
-        
+
+
         // We add Thymeleaf's wrapper on top of the SpringSecurity basic evaluation context
         final EvaluationContext wrappedEvaluationContext =
                 SpringVersionSpecificUtils.wrapEvaluationContext(evaluationContext, contextVariables);
 
-        
+
         if (ExpressionUtils.evaluateAsBoolean(expressionObject, wrappedEvaluationContext)) {
 
             if (logger.isTraceEnabled()) {
                 logger.trace("[THYMELEAF][{}] Checked authorization using access expression \"{}\" for user \"{}\". Access GRANTED.",
                         new Object[] {TemplateEngine.threadIndex(), accessExpression, (authentication == null? null : authentication.getName())});
             }
-            
+
             return true;
-            
+
         }
 
         if (logger.isTraceEnabled()) {
             logger.trace("[THYMELEAF][{}] Checked authorization using access expression \"{}\" for user \"{}\". Access DENIED.",
                     new Object[] {TemplateEngine.threadIndex(), accessExpression, (authentication == null? null : authentication.getName())});
         }
-        
+
         return false;
-    
+
     }
-    
-    
-    
-    
-    private static SecurityExpressionHandler<?> getExpressionHandler(final ServletContext servletContext) {
+
+
+
+
+    @SuppressWarnings("unchecked")
+    private static SecurityExpressionHandler<FilterInvocation> getExpressionHandler(final ServletContext servletContext) {
 
         final ApplicationContext ctx =
                 WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext);
-        
+
         final Map<String, SecurityExpressionHandler> expressionHandlers =
                 ctx.getBeansOfType(SecurityExpressionHandler.class);
 
-        if (expressionHandlers.size() == 0) {
-            throw new TemplateProcessingException(
-                    "No visible SecurityExpressionHandler instance could be found in the application " +
-                    "context. There must be at least one in order to support expressions in Spring Security " +
-                    "authorization queries.");
+        for (SecurityExpressionHandler handler : expressionHandlers.values()) {
+            if (FilterInvocation.class.equals(GenericTypeResolver.resolveTypeArgument(handler.getClass(), SecurityExpressionHandler.class))) {
+                return handler;
+            }
         }
 
-        return (SecurityExpressionHandler) expressionHandlers.values().toArray()[0];
-        
+        throw new TemplateProcessingException(
+                "No visible SecurityExpressionHandler instance could be found in the application " +
+                "context. There must be at least one in order to support expressions in Spring Security " +
+                "authorization queries.");
+
     }
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
     public static boolean authorizeUsingUrlCheck(
-            final String url, final String method, final Authentication authentication, 
+            final String url, final String method, final Authentication authentication,
             final HttpServletRequest request, final ServletContext servletContext) {
-        
+
         if (logger.isTraceEnabled()) {
             logger.trace("[THYMELEAF][{}] Checking authorization for URL \"{}\" and method \"{}\" for user \"{}\".",
                     new Object[] {TemplateEngine.threadIndex(), url, method, (authentication == null? null : authentication.getName())});
         }
-        
+
         final boolean result =
                 getPrivilegeEvaluator(servletContext).isAllowed(
-                    request.getContextPath(), url, method, authentication) ? 
+                    request.getContextPath(), url, method, authentication) ?
                             true : false;
 
         if (logger.isTraceEnabled()) {
@@ -299,22 +302,22 @@ public final class AuthUtils {
                     (result? "Access GRANTED." : "Access DENIED."),
                     new Object[] {TemplateEngine.threadIndex(), url, method, (authentication == null? null : authentication.getName())});
         }
-        
+
         return result;
-        
+
     }
 
 
-    
 
 
-    
+
+
     private static WebInvocationPrivilegeEvaluator getPrivilegeEvaluator(final ServletContext servletContext) {
 
         final ApplicationContext ctx =
                 WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext);
-        
-        final Map<String, WebInvocationPrivilegeEvaluator> privilegeEvaluators = 
+
+        final Map<String, WebInvocationPrivilegeEvaluator> privilegeEvaluators =
                 ctx.getBeansOfType(WebInvocationPrivilegeEvaluator.class);
 
         if (privilegeEvaluators.size() == 0) {
@@ -325,15 +328,15 @@ public final class AuthUtils {
         }
 
         return (WebInvocationPrivilegeEvaluator) privilegeEvaluators.values().toArray()[0];
-        
+
     }
 
 
 
-    
+
     public static ApplicationContext getContext(final ServletContext servletContext) {
         return WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext);
     }
-    
-    
+
+
 }
